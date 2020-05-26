198b3ce164c849632459c2419163ca2c3c912a50
username@email.com <1912339826@qq.com>
2020-05-26 14:36:28 +0800

    工具箱的建立(Utils.js)

--- dome/utils/Utils.js
+++ dome/utils/Utils.js
@@ -1,3 +1,17 @@
+// 工具箱
+
+// 链接参数提取
+// 截取url参数,传的参数为当前页面的window.location.search
+function LinkParameterExtraction(search) {
+    var obj = {};
+    var list = search.split('?')[1].split('&')
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+        obj[element.split('=')[0]] = element.split('=')[1]
+    }
+    return obj
+}
+
 // 设置html标签的字体大小即设置rem
 (function (doc, win) {
     var docEl = doc.documentElement,
