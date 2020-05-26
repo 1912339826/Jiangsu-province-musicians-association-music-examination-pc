9a23058b2d415a2ee3fab59c37cd71dd9c8cac41
username@email.com <1912339826@qq.com>
2020-05-26 11:26:25 +0800

    api.js文件的重写,请求格式的优化

--- dome/api/api.js
+++ dome/api/api.js
@@ -1,8 +1,47 @@
-function getAsk() {
+function getApi() {
     return {
-        "baseUrl": "a",
+        "baseUrl": "http://47.98.169.218:8090",
         "url": {
-            "name":"nameUrl"
+            "name": {
+                "nameUrl": "",
+                "type": "get/post"
+            },
+            "informationCategory": {
+                "nameUrl": "/information/category/list",
+                "type": "get"
+            }
         }
     }
 }
+
+// function Ask(nameUrl, type, data) {
+//     if (type == "get") {
+//         $.ajax({
+//             type: type,
+//             dataType: "json",
+//             url: `${getApi().baseUrl}${nameUrl}`,
+//             data: data,
+//             timeout: 5000,
+//             success: function (response) {
+//             },
+//             error: function (err) {
+//                 console.log("请求错误");
+//             }
+//         });
+//     }
+
+//     if (type == "post") {
+//         $.ajax({
+//             type: "POST",
+//             contentType: "application/json; charset=utf-8",
+//             dataType: "json",
+//             url: `${getApi.baseUrl}${nameUrl}`,
+//             data: data,
+//             success: function (response) {
+//             },
+//             error: function (err) {
+//                 console.log("请求错误");
+//             }
+//         });
+//     }
+// }
--- dome/src/js/index.js
+++ dome/src/js/index.js
@@ -1,27 +1,32 @@
 // 每个页面js必备,api的引入
-var api = getAsk()
-// import './main'
-// 页面加载之后执行的事件
-$(function () {
-    console.log(api)
-})
+// let informationCategory = Ask(getApi().url.informationCategory.nameUrl,"get")
+// if(informationCategory){
+//     console.log(informationCategory)
+// }
 
+var informationCategoryUrl = `${getApi().baseUrl}${getApi().url.informationCategory.nameUrl}`
 // 请求
-function dataFun(url) {
-    // $.ajax({
-    //     url:"",
-    //     type:"POST",
-    //     data:{"code":"bjpks"},
-    //     beforeSend:function(){
-    //         alert("请求前")
-    //     },
-    //     success:function (data) {
-    //        var data=JSON.parse(data);
-    //        console.log(data);
-    //        alert("ajax请求");
-    //     },
-    //     error:function () {
-    //      console.log("请求错误");
-    //     }
-    // });
+function informationCategory() {
+    $.ajax({
+        type: 'get',
+        dataType: "json",
+        url: informationCategoryUrl,
+        // data: data,
+        timeout: 5000,
+        success: function (response) {
+            console.log(response)
+            for (let index = 0; index < response.result.length; index++) {
+                const element = response.result[index];
+                var a = $("<a href=''></a>")
+                a.text(element.name)
+                a.attr('href', `./src/pages/er.html?id=${element.id}`)
+                $("section").append(a);
+            }
+        },
+        error: function (err) {
+            console.log("请求错误");
+            res = err
+        }
+    });
 }
+informationCategory()
